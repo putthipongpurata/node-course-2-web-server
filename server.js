24f982ba57c4fb6c8b2bed253164389a20e3da79
unknown <putthipongpurata@gmail.com>
2017-07-24 21:41:21 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+var app = express();
+app.use(favicon(__dirname + '/favicon.ico'));
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use(function(req, res, next){
+	let now = new Date().toString();
+	let log = `${now} : ${req.method} ${req.url}`;
+	fs.appendFile('server.log', log + '\n',(err) => {
+		if(err){console.log('Unable to append to server.log.');}
+	});
+	next();
+});
+
+// app.use((req, res, next) => {
+// 	res.render('maintenance.hbs');
+// });
+
+hbs.registerHelper('getCurrentYear',() => {
+	return new Date().getFullYear();
+});
+
+app.get('/',(req, res) => {
+	res.render('home.hbs',{
+		pageTitle: 'Home'
+	});
+});
+app.get('/about',(req, res) => {
+	res.render('about.hbs',{
+		pageTitle: 'About Pages'
+	});
+});
+
+app.listen(3000); 
